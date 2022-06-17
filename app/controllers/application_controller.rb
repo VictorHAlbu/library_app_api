8997ab996a8e27750f43effc23b29996de1206ee
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-06-17 10:46:53 -0300

    add name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
 end
